f8382d63dc2517fb38c1d3d49d7e32d7b956c2b8
Andrés Sarmiento <Sarmiento@amatea.org>
2016-11-17 10:35:34 -0500

    añadiendo thum Aves

--- dbconnection/aveconection.js
+++ dbconnection/aveconection.js
@@ -21,6 +21,7 @@ var AveSchema = new Schema({
   actividad_ave: String,
   coordenada: String,
   canto: String,
+  thumbUrl: String,
   foto_macho: String,
   foto_hembra: String,
   foto1: String,
